31b77077b1b05e8fcc5114c2c7b317c12131e797
AntekW34 <antekw34@gmail.com>
2026-05-24 00:07:47 +0200

    added set_text to sattelites

--- scenes/satelite.gd
+++ scenes/satelite.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export_node_path("Node3D") var sun_node
 
 @onready var wing_holder: MeshInstance3D = $"wing holder"
+@onready var label_3d: Label3D = $Label3D
 
 var _sun: Node3D
 var data: SatelliteData = null
@@ -12,7 +13,9 @@ func _ready() -> void:
 	if _sun == null and sun_node != NodePath():
 		_sun = get_node_or_null(sun_node)
 
-
+func set_country_text(text:String)->void: #use on init
+	label_3d.text = text
+	
 func set_sun(node: Node3D) -> void:
 	_sun = node
 
